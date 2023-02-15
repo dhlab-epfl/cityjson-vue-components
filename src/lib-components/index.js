0f8fadc931676857d4238c1c152136a919e343ec
dddpt <didier.dupertuis@epfl.ch>
2023-02-15 12:03:15 +0100

    adding common/ folder to exports

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -3,3 +3,5 @@ export { default as CityObjectInfo } from './CityObjectInfo.vue';
 export { default as CityObjectsTree } from './CityObjectsTree.vue';
 export { default as CityObjectsTreeItem } from './CityObjectsTreeItem.vue';
 export { default as ThreeJsViewer } from './ThreeJsViewer.vue';
+export { default as ExpandableBadge } from './common/ExpandableBadge.vue';
+export { default as GeometryBadge } from './common/GeometryBadge.vue';
